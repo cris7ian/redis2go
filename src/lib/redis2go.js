0037f8b6e0136548b3509cf9ecf4fef26d8a537b
Cristian Caroli <>
2018-05-27 11:54:05 +0200

    zadd, zcard, zrank, zrange commands

--- src/lib/redis2go.js
+++ src/lib/redis2go.js
@@ -1,4 +1,5 @@
 const values = require('lodash').values
+const chunk = require('lodash').chunk
 class Redis2Go {
   constructor() {
     this.db = {}
@@ -6,17 +7,17 @@ class Redis2Go {
   }
 
   set(key, value, secondsToExpire) {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       this.db[key] = value
-      if(secondsToExpire) {
-        setTimeout(() => this.del(key), secondsToExpire*1000)
+      if (secondsToExpire) {
+        setTimeout(() => this.del(key), secondsToExpire * 1000)
       }
       resolve('OK')
     })
   }
 
   get(key) {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       const value = this.db[key]
       if (value !== undefined) {
         resolve(value)
@@ -32,12 +33,14 @@ class Redis2Go {
         delete this.db[arguments[0]]
         resolve('OK')
       } else {
-        resolve(values(arguments).reduce((accumulator, arg)=> {
-          if (this.db[arg] !== undefined) {
-            delete this.db[arg]
-            return accumulator + 1
-          }
-        }, 0))
+        resolve(
+          values(arguments).reduce((accumulator, arg) => {
+            if (this.db[arg] !== undefined) {
+              delete this.db[arg]
+              return accumulator + 1
+            }
+          }, 0)
+        )
       }
     })
   }
@@ -56,11 +59,83 @@ class Redis2Go {
       } else if (parseInt(this.db[key])) {
         this.set(key, parseInt(this.db[key]) + 1)
         resolve(this.get(key))
-      } else  {
+      } else {
         reject('Value stored cannot be represented as an integer')
       }
     })
   }
+
+  zadd() {
+    return new Promise((resolve, reject) => {
+      let element
+      const key = arguments[0]
+      if (this.db[key] !== undefined) {
+        if (!this.db[key].zset) {
+          return reject('element is not a zset')
+        }
+        element = this.db[key]
+      } else {
+        element = {
+          zset: true,
+          set: new Set(),
+          order: {}
+        }
+      }
+      const instructions = chunk(values(arguments).slice(1), 2)
+      instructions.map(([order, value]) => {
+        element.set.add(value)
+        element.order[value] = order
+      })
+      this.db[key] = element
+      resolve(instructions.length)
+    })
+  }
+
+  zcard(key) {
+    return new Promise(resolve => {
+      if (this.db[key] === undefined || !this.db[key].zset) {
+        resolve(0)
+      } else {
+        resolve(this.db[key].set.size)
+      }
+    })
+  }
+
+  zrank(key, value) {
+    return new Promise(resolve => {
+      const element = this.db[key]
+      const hasValue =
+        element !== undefined && element.zset && element.set.has(value)
+      if (!hasValue) {
+        resolve(null)
+      } else {
+        const sortedSet = Array.from(element.set)
+          .sort(object => element.order[object])
+          .reverse()
+        const rank = sortedSet
+          .findIndex(orderedValue => orderedValue === value)
+        resolve(rank)
+      }
+    })
+  }
+
+  zrange(key, start, end) {
+    return new Promise((resolve,reject) => {
+      const element = this.db[key]
+      const hasValue =
+        element !== undefined && element.zset
+      if (!hasValue) {
+        reject('Element is not a set')
+      } else {
+        const sortedSet = Array.from(element.set)
+          .sort(object => element.order[object])
+          .reverse()
+        const endIndex = end < 0 ? sortedSet.length - end : end
+        resolve(sortedSet.slice(start, endIndex))
+      }
+    })
+  }
+
 }
 
 module.exports = Redis2Go
--- src/lib/redis2go.test.js
+++ src/lib/redis2go.test.js
@@ -1,7 +1,6 @@
 const Redis2Go = require('./redis2go')
 
 describe('Redis2Go Commands', () => {
-
   it('should set a key', done => {
     const redis = new Redis2Go()
     redis.set(100).then(result => {
@@ -10,16 +9,14 @@ describe('Redis2Go Commands', () => {
     })
   })
 
-
   it('should set a key and get it', done => {
     const redis = new Redis2Go()
-    redis.set('my_key', 100)
-      .then(() => {
-        redis.get('my_key').then(result => {
-          expect(result).toBe(100)
-          done()
-        })
+    redis.set('my_key', 100).then(() => {
+      redis.get('my_key').then(result => {
+        expect(result).toBe(100)
+        done()
       })
+    })
   })
 
   it('should delete a key already set', done => {
@@ -34,10 +31,10 @@ describe('Redis2Go Commands', () => {
 
   it('should set a key and this key should expire after a second', done => {
     const redis = new Redis2Go()
-    redis.set('my_key', 100, 1)
+    redis
+      .set('my_key', 100, 1)
       .then(() => new Promise(resolve => setTimeout(() => resolve(), 1100)))
       .then(() => {
-        console.log('hey!')
         redis.get('my_key').then(result => {
           expect(result).toBe(null)
           done()
@@ -47,7 +44,8 @@ describe('Redis2Go Commands', () => {
 
   it('should delete several keys that already exist', done => {
     const redis = new Redis2Go()
-    redis.set('my_key', 100)
+    redis
+      .set('my_key', 100)
       .then(() => redis.set('my_other_key', 200))
       .then(() => {
         redis.del('my_key', 'my_other_key').then(result => {
@@ -59,7 +57,8 @@ describe('Redis2Go Commands', () => {
 
   it('Should return the size of my db', done => {
     const redis = new Redis2Go()
-    redis.set('my_key', 100)
+    redis
+      .set('my_key', 100)
       .then(() => redis.set('my_other_key', 200))
       .then(() => {
         redis.dbSize().then(size => {
@@ -69,7 +68,7 @@ describe('Redis2Go Commands', () => {
       })
   })
 
-  it('should increment my index if I don\'t have one', done => {
+  it("should increment my index if I don't have one", done => {
     const redis = new Redis2Go()
     redis.incr('my_key').then(result => {
       expect(result).toBe(1)
@@ -79,25 +78,144 @@ describe('Redis2Go Commands', () => {
 
   it('should increment my index by 1 if have one', done => {
     const redis = new Redis2Go()
-    redis.set('my_key', 100)
-      .then(() => {
-      redis.incr('my_key').then(result => {
-        expect(result).toBe(101)
-        done()
-      })
+    redis.set('my_key', 100).then(() => {
+      redis
+        .incr('my_key')
+        .then(result => {
+          expect(result).toBe(101)
+          done()
+        })
         .catch(() => done())
     })
   })
 
   it('should not be able to increment an index by 1 if have a different type', done => {
     const redis = new Redis2Go()
-    redis.set('my_key', 'not a number')
-      .then(() => {
-        redis.incr('my_key')
-          .catch(error => {
-            expect(error).toBe('Value stored cannot be represented as an integer')
-            done()
-          })
+    redis.set('my_key', 'not a number').then(() => {
+      redis.incr('my_key').catch(error => {
+        expect(error).toBe('Value stored cannot be represented as an integer')
+        done()
       })
+    })
+  })
+
+  it('should create a sorted set and insert one element', done => {
+    const redis = new Redis2Go()
+    redis.zadd('myzset', 1, 'one').then(result => {
+      expect(result).toBe(1)
+      done()
+    })
+  })
+
+  it('should create a sorted set and insert two elements', done => {
+    const redis = new Redis2Go()
+    redis.zadd('myzset', 1, 'one', 2, 'two').then(result => {
+      expect(result).toBe(2)
+      done()
+    })
+  })
+
+  it('should add another element to my sorted set', done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two')
+    ]).then(() => {
+      redis.get('myzset').then(element => {
+        expect(element.set.size).toBe(2)
+        done()
+      })
+    })
+  })
+
+  it('should return the cardinality of my sorted set', done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two')
+    ]).then(() => {
+      redis.zcard('myzset').then(size => {
+        expect(size).toBe(2)
+        done()
+      })
+    })
+  })
+
+  it('should return zero if there is no set with that key', done => {
+    const redis = new Redis2Go()
+    redis.zcard('no_set').then(size => {
+      expect(size).toBe(0)
+      done()
+    })
+  })
+
+  it('should give me the order of the third element', done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two'),
+      redis.zadd('myzset', 3, 'three')
+    ]).then(() => {
+      redis.zrank('myzset', 'three').then(rank => {
+        expect(rank).toBe(2)
+        done()
+      })
+    })
+  })
+
+  it("should return null because the value isn't in the zset", done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two'),
+      redis.zadd('myzset', 3, 'three')
+    ]).then(() => {
+      redis.zrank('myzset', 'four').then(rank => {
+        expect(rank).toBe(null)
+        done()
+      })
+    })
+  })
+
+  it("should return the full range of my zset", done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two'),
+      redis.zadd('myzset', 3, 'three')
+    ]).then(() => {
+      redis.zrange('myzset', 0, -1).then(range => {
+        expect(range).toEqual(['one', 'two', 'three'])
+        done()
+      })
+    })
+  })
+
+  it("should return the element between 2 and 3", done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two'),
+      redis.zadd('myzset', 3, 'three')
+    ]).then(() => {
+      redis.zrange('myzset', 2, 3).then(range => {
+        expect(range).toEqual(['three'])
+        done()
+      })
+    })
+  })
+
+  it("should return the last 2 elements", done => {
+    const redis = new Redis2Go()
+    Promise.all([
+      redis.zadd('myzset', 1, 'one'),
+      redis.zadd('myzset', 2, 'two'),
+      redis.zadd('myzset', 3, 'three')
+    ]).then(() => {
+      redis.zrange('myzset', -2, -1).then(range => {
+        expect(range).toEqual(['two', 'three'])
+        done()
+      })
+    })
   })
 })
